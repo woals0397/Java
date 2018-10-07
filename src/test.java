dee0516ba05e5168bb886d59305e835abc2b3168
KimJaeMin <woals0397@naver.com>
2018-10-08 02:01:32 +0900

    adding files

--- src/test.java
+++ src/test.java
@@ -3,7 +3,5 @@ public class test {
         // Runnable 함수형인터페이스
         Thread thread = new Thread(() -> {System.out.println(Thread.currentThread() + "running"); System.out.println("AA");});
         thread.start();
-
-
     }
 }
