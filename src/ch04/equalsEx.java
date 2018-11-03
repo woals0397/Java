d280937da21aa9b460696e4fd1469b1eb2c14015
KimJaeMin <woals0397@naver.com>
2018-11-04 02:11:12 +0900

    Java

---
+++ src/ch04/equalsEx.java
@@ -0,0 +1,9 @@
+package ch04;
+
+public class equalsEx {
+    public static void main(String[] args) {
+        String a = null;
+
+        //System.out.println(a.equals(null));  NullPointerException발생
+    }
+}
